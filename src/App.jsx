75f754f9ffc93fb5b40158845e90007a56ad701a
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2023-01-13 14:00:24 +0900

    Aboutページの作成

--- src/App.jsx
+++ src/App.jsx
@@ -39,8 +39,52 @@ function App() {
           <h1 className='text-2xl sm:text-3xl font-medium mb-2 text-gray-900'>About me</h1>
           <p className='pb-10'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti eos soluta odio, consectetur obcaecati iure laboriosam itaque? Unde, accusantium ratione.</p>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident eum laboriosam nobis temporibus enim alias voluptatem soluta pariatur iure adipisci asperiores natus est optio quo dignissimos veniam assumenda nulla, quidem ducimus laudantium culpa sequi? Corrupti ea quidem consequatur voluptas magnam. Vero perspiciatis molestias iusto soluta placeat eligendi suscipit deleniti quod, facere reprehenderit corporis unde in ducimus nihil consequuntur quidem, rerum qui. Dolores necessitatibus exercitationem aspernatur a accusamus perspiciatis aliquam accusantium in modi magnam qui tenetur repellat rem ipsum saepe quas, inventore delectus excepturi maxime, fuga, fugit architecto doloribus consequuntur. Dolore sed id dolor illo quod veritatis est cumque reiciendis commodi?</p>
+        
+        <div className='flex flex-wrap'>
+          <div className='md:w-1/3 p-4'>
+            <div className='bg-gray-200 rounded-lg p-8'>
+              <div className='flex items-center mb-3'>
+                <div className='bg-green-500 text-white rounded-full'>
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9 21.5L17.5 13L13 10L15 2.5L6.5 11L11 14L9 21.5Z" fill="currentColor" /></svg>
+                </div>
+                <h2 className='text-gray-900 text-lg font-medium ml-2'>web Developer</h2>
+               </div>
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde doloremque alias delectus quasi, eaque aliquam quidem vitae odit placeat blanditiis.</p>
+               <a href='#' className='flex mt-3 text-green-500 items-center'>もっと見る
+               <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15.0378 6.34317L13.6269 7.76069L16.8972 11.0157L3.29211 11.0293L3.29413 13.0293L16.8619 13.0157L13.6467 16.2459L15.0643 17.6568L20.7079 11.9868L15.0378 6.34317Z" fill="currentColor" /></svg>
+               </a>
+            </div>
+          </div>
+          <div className='md:w-1/3 p-4'>
+            <div className='bg-gray-200 rounded-lg p-8'>
+              <div className='flex items-center mb-3'>
+                <div className='bg-green-500 text-white rounded-full'>
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9 21.5L17.5 13L13 10L15 2.5L6.5 11L11 14L9 21.5Z" fill="currentColor" /></svg>
+                </div>
+                <h2 className='text-gray-900 text-lg font-medium ml-2'>web Developer</h2>
+               </div>
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde doloremque alias delectus quasi, eaque aliquam quidem vitae odit placeat blanditiis.</p>
+               <a href='#' className='flex mt-3 text-green-500 items-center'>もっと見る
+               <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15.0378 6.34317L13.6269 7.76069L16.8972 11.0157L3.29211 11.0293L3.29413 13.0293L16.8619 13.0157L13.6467 16.2459L15.0643 17.6568L20.7079 11.9868L15.0378 6.34317Z" fill="currentColor" /></svg>
+               </a>
+            </div>
+          </div>
+          <div className='md:w-1/3 p-4'>
+            <div className='bg-gray-200 rounded-lg p-8'>
+              <div className='flex items-center mb-3'>
+                <div className='bg-green-500 text-white rounded-full'>
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9 21.5L17.5 13L13 10L15 2.5L6.5 11L11 14L9 21.5Z" fill="currentColor" /></svg>
+                </div>
+                <h2 className='text-gray-900 text-lg font-medium ml-2'>web Developer</h2>
+               </div>
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde doloremque alias delectus quasi, eaque aliquam quidem vitae odit placeat blanditiis.</p>
+               <a href='#' className='flex mt-3 text-green-500 items-center'>もっと見る
+               <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15.0378 6.34317L13.6269 7.76069L16.8972 11.0157L3.29211 11.0293L3.29413 13.0293L16.8619 13.0157L13.6467 16.2459L15.0643 17.6568L20.7079 11.9868L15.0378 6.34317Z" fill="currentColor" /></svg>
+               </a>
+            </div>
+          </div>
+          </div>
         </div>
-        <div></div>
       </div>
     </section>
     </>
