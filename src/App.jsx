de88fd67fe51fb3291b6baa64ac3f8819449eead
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2023-01-13 11:47:14 +0900

    mainの作成

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,10 @@ import './App.css'
 
 function App() {
   return (
+    <>
     <header className='text-gray-700 border-b border-gray-200'>
       <div className='container flex mx-auto p-5 flex-col md:flex-row items-center'>
-        <a href='#' className='font-medium text-gray-900'>
+        <a href='#' className='font-medium text-gray-900 mb-4 md:mb-0'>
           <span className='text-xl ml-3'>kou-port</span>
         </a>
         <nav className='md:ml-auto text-base'>
@@ -17,6 +18,19 @@ function App() {
         </nav>
       </div>
     </header>
+    <section>
+      <div>
+        <div>
+          <h1>
+            Hi<br/>im,kou<br/>test<br/>
+          </h1>
+          <p></p>
+          <button></button>
+        </div>
+        <div></div>
+      </div>
+    </section>
+    </>
   )
 }
 export default App
