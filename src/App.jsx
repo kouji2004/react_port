4da784e40b599c8e6100cdde6ae1991a740212ab
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2023-01-13 10:28:27 +0900

    hederの作成

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,15 @@ import './App.css'
 function App() {
   return (
     <header className='text-gray-700 border-b border-gray-200'>
-      <div className='container flex mx-auto p-5 flex-col md:flex-row'>
-        <a href='#'>kou-port</a>
-        <nav>
-          <a href='home'>Home</a>
-          <a href='About'>About</a>
-          <a href='Skills'>Skills</a>
-          <a href='Blog'>Blog</a>
+      <div className='container flex mx-auto p-5 flex-col md:flex-row items-center'>
+        <a href='#' className='font-medium text-gray-900'>
+          <span className='text-xl ml-3'>kou-port</span>
+        </a>
+        <nav className='md:ml-auto text-base'>
+          <a href='home' className='mr-5 hover:text-blue-400 duration'>Home</a>
+          <a href='About' className='mr-5 hover:text-blue-400 duration'>About</a>
+          <a href='Skills'  className='mr-5 hover:text-blue-400 duration'>Skills</a>
+          <a href='Blog'  className='mr-5 hover:text-blue-400 duration'>Blog</a>
         </nav>
       </div>
     </header>
