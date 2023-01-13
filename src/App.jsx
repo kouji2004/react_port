6ef3472ab3821f09abfc71a8f9a1126d52b3fdd2
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2023-01-13 12:12:00 +0900

    mainの作成

--- src/App.jsx
+++ src/App.jsx
@@ -18,16 +18,16 @@ function App() {
         </nav>
       </div>
     </header>
-    <section>
-      <div>
-        <div>
-          <h1>
+    <section className='text-gray-700' id='home'>
+      <div className='flex container mx-auto py-20 px-5 flex-col md:flex-row items-center'>
+        <div className='md:w-1/2 flex-grow mb-16 lg:pr-24 md:pr-16 text-center md:text-left'>
+          <h1 className='text-3xl sm:text-6x1 text-gray-900 font-medium mb-4'>
             Hi<br/>im,kou<br/>test<br/>
           </h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus quasi sint fugiat totam tempore, aperiam iste reprehenderit illo adipisci magni.</p>
-          <button>Contact Me</button>
+          <p className='mb-8 leading-relaxed'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus quasi sint fugiat totam tempore, aperiam iste reprehenderit illo adipisci magni.</p>
+          <button className='text-white bg-green-500 py-2 px-6 border-0 rounded text-lg hover:bg-green-600 duration-300'>Contact Me</button>
         </div>
-        <div>
+        <div className='md:w-1/2 lg:max-w-lg w-5/6'>
           <img src="./img/korobanaii.jpg"></img>
         </div>
       </div>
