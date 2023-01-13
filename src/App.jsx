0ac804d0054727c88eb1ef534d9b76ff8e448b15
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2023-01-13 10:05:33 +0900

    hederの作成

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,17 @@ import './App.css'
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <header className='text-gray-700 border-b border-gray-200'>
+      <div>
+        <a href='#'>kou-port</a>
+        <nav>
+          <a href='home'>Home</a>
+          <a href='About'>About</a>
+          <a href='Skills'>Skills</a>
+          <a href='Blog'>Blog</a>
+        </nav>
+      </div>
+    </header>
   )
 }
 export default App
